e1a408c5b99d51e60bfc811c3aa2ecd5d8b0c801
PabloSantiagoConde <pablosantiagoconde@gmail.com>
2024-10-07 15:16:01 +0000

    ADD: Se agrega la resolución del punto E

---
+++ RTA_SCRIPT_Examen_20241006/Punto_E.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Filtrando la info de la memoria RAM y creando un .txt"
+
+grep MemTotal /proc/meminfo > ~/repogit/UTNFRA_SO_1P2C_2024_Conde-/RTA_ARCHIVOS_Examen_20241006/Filtro_Basico.txt
+
+echo "Filtrando la info de chasis y guardandola en un .txt"
+
+sudo dmidecode -t chassis | grep Manufacturer >> Filtro_Basico.txt
+
