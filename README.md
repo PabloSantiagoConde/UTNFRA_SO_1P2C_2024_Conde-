abf1b8212bd8414087e38a89a733403e3f40c5a0
PabloSantiagoConde <pablosantiagoconde@gmail.com>
2024-10-07 18:18:34 +0000

    docs: Se modifica el readme con los datos solicitados

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+
 # UTNFRA_SO_1P2C_2024_Conde-
+
+Nombre: Pablo 
+Apellido: Conde
+Legajo: 117645
+División:318
--- RTA_SCRIPT_Examen_20241006/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241006/Punto_B.sh
@@ -55,7 +55,7 @@ $PART_SIZE
 w
 EOF
 NUEVO
-[1:37]
+
 echo
 echo "Mostrar particionamientos"
 sudo fdisk $DISCO_TP -l
@@ -63,12 +63,12 @@ sudo fdisk $DISCO_TP -l
 echo
 echo "Formateando las particiones"
 
-Formateo de las particiones primarias
+echo "Formateo de las particiones primarias"
 sudo mkfs.ext4 ${DISCO_TP}1
 sudo mkfs.ext4 ${DISCO_TP}2
 sudo mkfs.ext4 ${DISCO_TP}3
 
-Formateo de las particiones lógicas (5 a 11)
+echo "Formateo de las particiones lógicas (5 a 11)"
 sudo mkfs.ext4 ${DISCO_TP}5
 sudo mkfs.ext4 ${DISCO_TP}6
 sudo mkfs.ext4 ${DISCO_TP}7
@@ -80,15 +80,16 @@ sudo mkfs.ext4 ${DISCO_TP}11
 echo
 echo "Montando de forma PERSISTENTE"
 
-echo UUID=38686a72-b4c0-4c93-8392-2c12e74cc290  /Examenes-UTN/alumno_1/parcial_1  ext4  defaults  0  2 | sudo tee -a /etc/fstab
-echo PARTUUID=ecdb58b0-0b  /Examenes-UTN/alumno_1/parcial_2  ext4  defaults  0  2 | sudo tee -a /etc/fstab
-echo PARTUUID=ecdb58b0-09  /Examenes-UTN/alumno_1/parcial_3  ext4  defaults  0  2 | sudo tee -a /etc/fstab
-echo PARTUUID=ecdb58b0-07  /Examenes-UTN/alumno_2/parcial_2  ext4  defaults  0  2 | sudo tee -a /etc/fstab
-echo PARTUUID=ecdb58b0-05  /Examenes-UTN/alumno_2/parcial_3  ext4  defaults  0  2 | sudo tee -a /etc/fstab
-echo UUID=37c58960-7765-4376-b5e6-aff2e8dd9283  /Examenes-UTN/alumno_3/parcial_1  ext4  defaults  0  2 | sudo tee -a /etc/fstab
-echo PARTUUID=ecdb58b0-0a  /Examenes-UTN/alumno_3/parcial_2  ext4  defaults  0  2 | sudo tee -a /etc/fstab
-echo PARTUUID=ecdb58b0-08  /Examenes-UTN/alumno_3/parcial_3  ext4  defaults  0  2 | sudo tee -a /etc/fstab
-echo PARTUUID=ecdb58b0-06  /Examenes-UTN/profesores          ext4  defaults  0  2 | sudo tee -a /etc/fstab
+echo "${sudo lsblk -no UUID /dev/sdc1}  /Examenes-UTN/alumno_1/parcial_1  ext4  defaults  0  2" | sudo tee -a /etc/fstab
+echo "${sudo lsblk -no UUID /dev/sdc2}  /Examenes-UTN/alumno_1/parcial_2  ext4  defaults  0  2" | sudo tee -a /etc/fstab
+echo "${sudo lsblk -no UUID /dev/sdc3}  /Examenes-UTN/alumno_1/parcial_3  ext4  defaults  0  2" | sudo tee -a /etc/fstab
+echo "${sudo lsblk -no UUID /dev/sdc11} /Examenes-UTN/alumno_2/parcial_1  ext4  defaults  0  2" | sudo tee -a /etc/fstab
+echo "${sudo lsblk -no UUID /dev/sdc5}  /Examenes-UTN/alumno_2/parcial_2  ext4  defaults  0  2" | sudo tee -a /etc/fstab
+echo "${sudo lsblk -no UUID /dev/sdc6}  /Examenes-UTN/alumno_2/parcial_3  ext4  defaults  0  2" | sudo tee -a /etc/fstab
+echo "${sudo lsblk -no UUID /dev/sdc7}  /Examenes-UTN/alumno_3/parcial_1  ext4  defaults  0  2" | sudo tee -a /etc/fstab
+echo "${sudo lsblk -no UUID /dev/sdc8}  /Examenes-UTN/alumno_3/parcial_2  ext4  defaults  0  2" | sudo tee -a /etc/fstab
+echo "${sudo lsblk -no UUID /dev/sdc9}  /Examenes-UTN/alumno_3/parcial_3  ext4  defaults  0  2" | sudo tee -a /etc/fstab
+echo "${sudo lsblk -no UUID /dev/sdc10} /Examenes-UTN/profesores          ext4  defaults  0  2" | sudo tee -a /etc/fstab
 
 
 
